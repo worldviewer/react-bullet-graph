693a1d3aab519a321e875f53c781dc0c3d67e1d0
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-02-11 18:01:33 -0800

    Set up props for BulletGraph component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <BulletGraph />
+                <BulletGraph
+                    orientation="horizontal"
+                    title="Revenue 2005 YTD"
+                    textLabel="(U.S. $ in thousands)"
+                    scaleMin="0"
+                    scaleMax="300"
+                    performanceValue="275"
+                    symbolMarker="265"
+                    badVal="200"
+                    satisfactoryVal="250"
+                    scaleUnits="none" />
             </div>
         );
     }
--- src/BulletGraph/BulletGraph.js
+++ src/BulletGraph/BulletGraph.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './BulletGraph.css';
 
 class BulletGraph extends Component {
+	propTypes: {
+		orientation: React.PropTypes.string.isRequired,
+		title: React.PropTypes.string.isRequired,
+		textLabel: React.PropTypes.string.isRequired,
+		scaleMin: React.PropTypes.number.isRequired,
+		scaleMax: React.PropTypes.number.isRequired,
+		performanceValue: React.PropTypes.number,
+		symbolMarker: React.PropTypes.number,
+		badVal: React.PropTypes.number,
+		satisfactoryVal: React.PropTypes.number,
+		scaleUnits: React.PropTypes.string,
+		titleStyle: React.PropTypes.string
+	}
+
     render() {
         return (
             <div className="BulletGraph">
