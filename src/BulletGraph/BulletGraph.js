a0e9e6c0fee362379151449ffbc60d07e5816d82
Chris Reeve <paradigmsareconstructed@gmail.com>
2017-02-11 17:32:27 -0800

    Set up BulletGraph component

---
+++ src/BulletGraph/BulletGraph.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './BulletGraph.css';
+
+class BulletGraph extends Component {
+    render() {
+        return (
+            <div className="BulletGraph">
+            	<p>testing</p>
+            </div>
+        );
+    }
+}
+
+export default BulletGraph;
